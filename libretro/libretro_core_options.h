e3ff4668e408ddf1bf5b87a14f9acf9e2f8ced32
spluto-dot <brunoraji@ymail.com>
2026-06-15 11:19:49 -0300

    Change OpenGL option to 'none' and resolution to '480x272'

--- libretro/libretro_core_options.h
+++ libretro/libretro_core_options.h
@@ -301,7 +301,7 @@ struct retro_core_option_v2_definition option_defs_us[] = {
          { "none",   "None" },
          { NULL, NULL },
       },
-      "opengl"
+      "none"
    },
    {
       "ppsspp_software_rendering",
@@ -333,7 +333,7 @@ struct retro_core_option_v2_definition option_defs_us[] = {
          { "4800x2720", "10x (4800x2720)" },
          { NULL, NULL },
       },
-      "1920x1088"
+      "480x272"
    },
    {
       "ppsspp_mulitsample_level",
